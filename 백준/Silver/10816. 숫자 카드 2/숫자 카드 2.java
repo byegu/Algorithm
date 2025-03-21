7b7f6fcef840ff97f70800a9334ec27da94f0fab
byegu <100301088+byegu@users.noreply.github.com>
2025-03-21 20:21:04 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2280 ms, Memory: 358856 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int M = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int query = sc.nextInt();
+            sb.append(map.getOrDefault(query, 0)).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
