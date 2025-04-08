8fef793aa7fa02e2fed85a6f84ec3af612b68ba7
byegu <100301088+byegu@users.noreply.github.com>
2025-04-08 19:48:25 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 172 ms, Memory: 17472 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int V = scanner.nextInt();
+
+        int count = (V - B) / (A - B);
+
+        if ((V - B) % (A - B) != 0) {
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
