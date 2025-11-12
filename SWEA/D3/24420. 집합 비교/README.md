e67facb6926fda2921ad5f5008178f25e44c828d
byegu <100301088+byegu@users.noreply.github.com>
2025-11-12 15:32:39 +0900

    [D3] Title: 집합 비교, Time: 82 ms, Memory: 25,344 KB -BaekjoonHub

---
+++ SWEA/D3/24420. 집합 비교/README.md
@@ -0,0 +1,15 @@
+# [D3] 집합 비교 - 24420 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZcKaddqidnHBITY) 
+
+### 성능 요약
+
+메모리: 25,344 KB, 시간: 82 ms, 코드길이: 2,629 Bytes
+
+### 제출 일자
+
+2025-11-12 15:32
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/24420. 집합 비교/집합 비교.java
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			List<Integer> A = new ArrayList<Integer>();
+			List<Integer> B = new ArrayList<Integer>();
+			
+			int a_size = Integer.parseInt(st.nextToken());
+			int b_size = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(br.readLine());
+			for(int n = 0; n < a_size; n++) {
+				A.add(Integer.parseInt(st.nextToken()));
+			}
+			st = new StringTokenizer(br.readLine());
+			for(int n = 0; n < b_size; n++) {
+				B.add(Integer.parseInt(st.nextToken()));
+			}
+			
+			if (a_size == b_size && A.containsAll(B) && B.containsAll(A)) {
+				System.out.println("=");
+			} else if (B.containsAll(A)) {
+				System.out.println("<");
+			} else if (A.containsAll(B)) {
+				System.out.println(">");
+			} else {
+				System.out.println("?");
+			}
+		}
+	}
+
+}
