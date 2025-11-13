c44d631b5eafb35ee51817312dce0c5f39dd55b2
byegu <100301088+byegu@users.noreply.github.com>
2025-11-13 20:27:19 +0900

    [D3] Title: 색상환, Time: 77 ms, Memory: 24,192 KB -BaekjoonHub

--- SWEA/D3/23003. 색상환/색상환.java
+++ SWEA/D3/23003. 색상환/색상환.java
@@ -1,74 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int t = 0; t < T; t++) {
-			String A, B;
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			A = st.nextToken();
-			B = st.nextToken();
-			
-			if (A.equals(B)) {
-				System.out.println("E");
-			} else if (A.equals("red")) {
-				if (B.equals("orange") || B.equals("purple")) {
-					System.out.println("A");
-				} else if (B.equals("green")) {
-					System.out.println("C");
-				} else {
-					System.out.println("X");
-				}
-			} else if (A.equals("orange")) {
-				if (B.equals("red") || B.equals("yellow")) {
-					System.out.println("A");
-				} else if (B.equals("blue")) {
-					System.out.println("C");
-				} else {
-					System.out.println("X");
-				}
-			} else if (A.equals("yellow")) {
-				if (B.equals("orange") || B.equals("green")) {
-					System.out.println("A");
-				} else if (B.equals("purple")) {
-					System.out.println("C");
-				} else {
-					System.out.println("X");
-				}
-			} else if (A.equals("green")) {
-				if (B.equals("yellow") || B.equals("blue")) {
-					System.out.println("A");
-				} else if (B.equals("red")) {
-					System.out.println("C");
-				} else {
-					System.out.println("X");
-				}
-			} else if (A.equals("blue")) {
-				if (B.equals("green") || B.equals("purple")) {
-					System.out.println("A");
-				} else if (B.equals("orange")) {
-					System.out.println("C");
-				} else {
-					System.out.println("X");
-				}
-			} else if (A.equals("purple")) {
-				if (B.equals("blue") || B.equals("red")) {
-					System.out.println("A");
-				} else if (B.equals("yellow")) {
-					System.out.println("C");
-				} else {
-					System.out.println("X");
-				}
-			}
-		}
-	}
+        String[] colors = {"red", "orange", "yellow", "green", "blue", "purple"};
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < colors.length; i++) {
+            map.put(colors[i], i);
+        }
+
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = map.get(st.nextToken());
+            int b = map.get(st.nextToken());
+
+            int diff = Math.abs(a - b);
+
+            if (diff == 0) {
+                System.out.println("E");
+            } else if (diff == 1 || diff == 5) {
+                System.out.println("A");
+            } else if (diff == 3) {
+                System.out.println("C");
+            } else {
+                System.out.println("X");
+            }
+        }
+    }
 }
