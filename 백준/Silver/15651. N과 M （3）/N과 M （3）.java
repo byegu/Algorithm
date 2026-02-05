d4b6286cf7b75ca93f916e1d908dccd9553e92cf
byegu <100301088+byegu@users.noreply.github.com>
2026-02-05 17:37:39 +0900

    [Silver III] Title: N과 M (3), Time: 544 ms, Memory: 208296 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N = 0;
+	static int M = 0;
+	static int sel[];
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		sel = new int[M];
+
+		perm(0);
+		System.out.println(sb.toString());
+	}
+
+	static void perm(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(sel[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			sel[depth] = i;
+			perm(depth + 1);
+		}
+	}
+}
