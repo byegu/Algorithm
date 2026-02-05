6b5c78fa9a7d7877a4463979f28a78c7bf41a9df
byegu <100301088+byegu@users.noreply.github.com>
2026-02-05 16:37:57 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 1,994 ms, Memory: 113,024 KB -BaekjoonHub

---
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/README.md
@@ -0,0 +1,15 @@
+# [D5] [S/W 문제해결 응용] 3일차 - 최적 경로 - 1247 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15OZ4qAPICFAYD) 
+
+### 성능 요약
+
+메모리: 113,024 KB, 시간: 1,994 ms, 코드길이: 1,647 Bytes
+
+### 제출 일자
+
+2026-02-05 16:37
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static class Point {
+		int x;
+		int y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	static int N = 0;
+
+	static Point[] arr;
+	static Point[] result;
+	static boolean[] visited;
+	static int min = Integer.MAX_VALUE;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			N = Integer.parseInt(br.readLine()) + 2;
+			min = Integer.MAX_VALUE;
+			arr = new Point[N];
+			result = new Point[N];
+			visited = new boolean[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			for (int i = 0; i < N; i++) {
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+
+				arr[i] = new Point(x, y);
+			}
+			
+			result[0] = new Point(arr[1].x, arr[1].y);
+			result[N-1] = new Point(arr[0].x, arr[0].y);
+			
+			perm(1);
+			System.out.println("#" + t + " " + min);
+		}
+	}
+
+	public static void perm(int depth) {
+		int dist = 0;
+		if (depth == N-1) {
+			for (int i = 0; i < result.length-1; i++) {
+				int x = Math.abs(result[i+1].x - result[i].x);
+				int y = Math.abs(result[i+1].y - result[i].y);
+				dist += x+y;
+			}
+			min = Math.min(min, dist);
+			return;
+		}
+		
+		for (int i = 2; i < arr.length; i++) {
+			if (visited[i]) {
+				continue;
+			}
+			
+			visited[i] = true;
+			result[depth] = new Point (arr[i].x, arr[i].y);
+			perm(depth+1);
+			visited[i] = false;
+		}
+	}
+}
