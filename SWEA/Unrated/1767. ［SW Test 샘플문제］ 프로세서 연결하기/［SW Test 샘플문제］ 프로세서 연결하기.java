5f23821565882551c11faa11655a6610ef3d59de
byegu <100301088+byegu@users.noreply.github.com>
2026-03-07 19:09:24 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 621 ms, Memory: 112,560 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -8,18 +8,16 @@ import java.util.StringTokenizer;
 public class Solution {
 	static int N;
 	static List<Core> cores;
-	static Core[] result;
+
 	static int[][] map;
 
 	static class Core {
 		int r;
 		int c;
-		boolean connect;
 
-		public Core(int r, int c, boolean connect) {
+		public Core(int r, int c) {
 			this.r = r;
 			this.c = c;
-			this.connect = connect;
 		}
 	}
 
@@ -47,7 +45,7 @@ public class Solution {
 					map[i][j] = temp;
 
 					if (temp == 1 && !(i == 0 || j == 0 || i == N - 1 || j == N - 1)) {
-						cores.add(new Core(i, j, false));
+						cores.add(new Core(i, j));
 					}
 				}
 			}
@@ -62,22 +60,24 @@ public class Solution {
 	}
 
 	static void simulate() {
-		int[][] newMap = new int[N][N];
 		int length = 0;
 		int coreCnt = 0;
 
-		result = new Core[cores.size()];
-
-		for (int i = 0; i < N; i++)
-			newMap[i] = map[i].clone();
+		List<int[]> wires = new ArrayList<>();
 
 		for (int i = 0; i < state.length; i++) {
-			if (state[i] == 4) continue;
+			if (state[i] == 4)
+				continue;
 
 			Core core = cores.get(i);
-			int added = connect(core, state[i], newMap, i);
+			int added = connect(core, state[i], wires);
 
-			if (added == -1) return;
+			if (added == -1) {
+				for (int[] w : wires) {
+					map[w[0]][w[1]] = 0;
+				}
+				continue;
+			}
 
 			coreCnt++;
 			length += added;
@@ -89,10 +89,13 @@ public class Solution {
 		} else if (coreCnt == maxCore) {
 			minLen = Math.min(minLen, length);
 		}
+		
+		for (int[] w : wires) {
+			map[w[0]][w[1]] = 0;
+		}
 	}
 
-	static int connect(Core core, int dir, int[][] map, int idx) {
-
+	static int connect(Core core, int dir, List<int[]> wires) {
 		int r = core.r;
 		int c = core.c;
 		List<int[]> path = new ArrayList<>();
@@ -101,31 +104,39 @@ public class Solution {
 			r += dr[dir];
 			c += dc[dir];
 
-			if (!isIn(r, c)) return -1;
+			if (!isIn(r, c))
+				return -1;
 
-			if (map[r][c] != 0) return -1;
+			if (map[r][c] != 0)
+				return -1;
 
 			path.add(new int[] { r, c });
 
 			if (r == 0 || c == 0 || r == N - 1 || c == N - 1) {
-				for (int[] p : path)
+				for (int[] p : path) {
 					map[p[0]][p[1]] = -1;
+					wires.add(p);
+				}
 
-				result[idx] = new Core(core.r, core.c, true);
 				return path.size();
 			}
 		}
 	}
 
 	static void dfs(int depth) {
-
 		int connected = 0;
-		for(int i=0;i<depth;i++)
-			if(state[i] != 4) connected++;
 
-		if(connected + (cores.size() - depth) < maxCore) return;
+		for (int i = 0; i < depth; i++)
+			if (state[i] != 4)
+				connected++;
+
+		// 현재 연결된 프로세서 + 남은 코어 수 < 최고 기록이면
+		// 남은 코어를 모두 연결해도 의미가 없기 때문에 return
+		if (connected + (cores.size() - depth) < maxCore)
+			return;
 
 		if (depth == cores.size()) {
+			// 모든 코어에 대해 방향을 정했다면 simulate
 			simulate();
 			return;
 		}
@@ -134,19 +145,20 @@ public class Solution {
 
 		for (int d = 0; d < 4; d++) {
 
-			if(!canConnect(core,d)) continue;
+			if (!canConnect(core, d))
+				continue;
 
+			// 연결 가능하면 프로세서 상태 연결 방향 저장
 			state[depth] = d;
 			dfs(depth + 1);
 		}
 
+		// 현재 프로세서를 연결하지 않고 다음 프로세서를 연결해보는 경우를 보기 위해 + 백트래킹
 		state[depth] = 4;
 		dfs(depth + 1);
 	}
 
-
 	static boolean canConnect(Core core, int dir) {
-
 		int r = core.r;
 		int c = core.c;
 
@@ -154,16 +166,17 @@ public class Solution {
 			r += dr[dir];
 			c += dc[dir];
 
-			if (!isIn(r, c)) return false;
+			if (!isIn(r, c))
+				return false;
 
-			if (map[r][c] != 0) return false;
+			if (map[r][c] != 0)
+				return false;
 
 			if (r == 0 || c == 0 || r == N - 1 || c == N - 1)
 				return true;
 		}
 	}
 
-	
 	static boolean isIn(int r, int c) {
 		return r >= 0 && r < N && c >= 0 && c < N;
 	}
