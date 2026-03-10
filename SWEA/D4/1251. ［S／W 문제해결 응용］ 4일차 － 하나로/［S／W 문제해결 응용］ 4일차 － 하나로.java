a2048cf634d3b0663881c0f2cae9e8620e2617ec
byegu <100301088+byegu@users.noreply.github.com>
2026-03-10 14:32:20 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 177 ms, Memory: 28,288 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,115 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-	static int T;
-	static int N; // 섬의 개수
 
-	static class Island {
-		int to;
-		double cost;
+    static int T, N;
+    static long[] x, y;
+    static double E;
 
-		public Island(int to, double cost) {
-			this.to = to;
-			this.cost = cost;
-		}
-	}
+    public static void main(String[] args) throws Exception {
 
-	static long[] ix;
-	static long[] iy;
-	static double[][] dist;
-	static double E; // 세율
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	static boolean[] visited;
-	static ArrayList<Island>[] graph;
-	static StringBuilder sb;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
 
-		T = Integer.parseInt(br.readLine());
+        for(int t=1;t<=T;t++){
 
-		for (int t = 1; t <= T; t++) {
-			N = Integer.parseInt(br.readLine());
-			graph = new ArrayList[N];
-			visited = new boolean[N];
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			sb = new StringBuilder();
-			sb.append("#" + t + " ");
-			
-			ix = new long[N];
-			iy = new long[N];
-			dist = new double[N][N];
+            N = Integer.parseInt(br.readLine());
 
-			for (int i = 0; i < N; i++) {
-				ix[i] = Long.parseLong(st.nextToken());
-				graph[i] = new ArrayList<>();
+            x = new long[N];
+            y = new long[N];
 
-			}
-			
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				iy[i] = Long.parseLong(st.nextToken());
-			}
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i=0;i<N;i++) x[i] = Long.parseLong(st.nextToken());
 
-			E = Double.parseDouble(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for(int i=0;i<N;i++) y[i] = Long.parseLong(st.nextToken());
 
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					long x1 = ix[i];
-					long y1 = iy[i];
+            E = Double.parseDouble(br.readLine());
 
-					long x2 = ix[j];
-					long y2 = iy[j];
+            sb.append("#").append(t).append(" ").append(Math.round(prim())).append("\n");
+        }
 
-					dist[i][j] = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-					dist[j][i] = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-				}
-			}
+        System.out.print(sb);
+    }
 
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					int from = i;
-					int to = j;
-					double cost = E * (dist[i][j] * dist[i][j]);
+    static double prim(){
 
-					graph[from].add(new Island(to, cost));
-					graph[to].add(new Island(from, cost));
-				}
-			}
+        boolean[] visited = new boolean[N];
+        double[] minEdge = new double[N];
 
-			prim();
-			
-			System.out.println(sb);
-		}
-	}
+        Arrays.fill(minEdge, Double.MAX_VALUE);
+        minEdge[0] = 0;
 
-	static void prim() {
-		PriorityQueue<Island> pq = new PriorityQueue<>((i1, i2) -> Double.compare(i1.cost, i2.cost));
-		pq.offer(new Island(0, 0));
-		
-		double result = 0;
-		while(!pq.isEmpty()) {
-			Island now = pq.poll();
-			
-			if (visited[now.to]) {
-				continue;
-			}
-			
-			result += now.cost;
-			visited[now.to] = true;
-			
-			for (Island next : graph[now.to]) {
-				if (!visited[next.to]) {
-					pq.offer(next);
-				}
-			}
-		}
-		
-		sb.append(Math.round(result));
-	}
+        double result = 0;
 
+        for(int i=0;i<N;i++){
+
+            double min = Double.MAX_VALUE;
+            int minVertex = -1;
+
+            for(int j=0;j<N;j++){
+                if(!visited[j] && minEdge[j] < min){
+                    min = minEdge[j];
+                    minVertex = j;
+                }
+            }
+
+            visited[minVertex] = true;
+            result += min;
+
+            for(int j=0;j<N;j++){
+                if(!visited[j]){
+                    long dx = x[minVertex] - x[j];
+                    long dy = y[minVertex] - y[j];
+
+                    double cost = E * (dx*dx + dy*dy);
+
+                    if(minEdge[j] > cost){
+                        minEdge[j] = cost;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
 }
