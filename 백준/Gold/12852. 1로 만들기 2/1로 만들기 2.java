e96805bee80b1c1274b7b202466e4fd8c4ded9d7
byegu <100301088+byegu@users.noreply.github.com>
2026-04-07 14:27:57 +0900

    [Gold V] Title: 1로 만들기 2, Time: 136 ms, Memory: 22168 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int dp[] = new int[N+1];
+		int path[] = new int[N+1];
+		
+		path[1] = 1;
+		
+		for (int i = 2; i < N+1; i++) {
+			dp[i] = dp[i-1] + 1;
+			path[i] = i-1;
+			
+			if (i % 2 == 0 && dp[i] > dp[i/2] + 1) {
+				dp[i] = dp[i/2] + 1;
+				path[i] = i/2;
+			}
+			
+			if (i % 3 == 0 && dp[i] > dp[i/3] + 1) {
+				dp[i] = dp[i/3] + 1;
+				path[i] =  i/3;
+			}
+		}
+		
+		System.out.println(dp[N]);
+		
+		int cur = N;
+		while(true) {
+			System.out.print(cur + " ");
+			if(cur == 1) break;
+			cur = path[cur];
+		}
+	}
+
+}
