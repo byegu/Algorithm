d50cecbfb60100f711601305ceab6ae26da2f958
byegu <100301088+byegu@users.noreply.github.com>
2026-04-08 15:19:18 +0900

    [Gold V] Title: 동전 2, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, K;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		int coin[] = new int[N];
+		int dp[] = new int[K + 1];
+
+		for (int i = 0; i < N; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
+		}
+
+		for (int i = 1; i < K + 1; i++) {
+			dp[i] = K + 1;
+		}
+
+		dp[0] = 0;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = coin[i]; j <= K; j++) {
+				dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
+			}
+		}
+
+		if (dp[K] == K+1) {
+			System.out.println(-1);
+		} else {
+			System.out.println(dp[K]);
+		}
+	}
+}
