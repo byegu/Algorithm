edfcae2a206c1fbc6d41d505c96cff95ed56edda
byegu <100301088+byegu@users.noreply.github.com>
2026-04-14 15:21:50 +0900

    [Gold IV] Title: 키 순서, Time: 532 ms, Memory: 37752 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int M;
-	static final int INF = 100_000_000;
-	static int[][] adj;
+	static final int INF = 100000000;
+	static int N, M;
+	static int dist[][];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,56 +15,49 @@ public class Main {
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		dist = new int[N + 1][N + 1];
 
-		adj = new int[N + 1][N + 1];
-
-		for (int i = 0; i <= N; i++) {
-			for (int j = 0; j <= N; j++) {
-				if (i == j)
-					continue;
-
-				adj[i][j] = INF;
-			}
+		for (int i = 1; i <= N; i++) {
+			Arrays.fill(dist[i], INF);
 		}
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 
-			adj[a][b] = 1;
+			dist[u][v] = 1;
 		}
-		floyd(adj);
-	}
-
-	static void floyd(int[][] adj) {
-		int count = 0;
 
 		for (int k = 1; k <= N; k++) {
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
-					if (adj[i][k] != INF && adj[k][j] != INF) {
-						adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+					if (dist[i][k] != INF && dist[k][j] != INF) {
+						dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
 					}
 				}
 			}
 		}
 
+		int result = 0;
+
 		for (int i = 1; i <= N; i++) {
 			int temp = 0;
 			for (int j = 1; j <= N; j++) {
-				if (i == j)
+				if (i == j) {
 					continue;
+				}
 
-				if (adj[i][j] != INF || adj[j][i] != INF) {
+				if (dist[i][j] != INF || dist[j][i] != INF) {
 					temp++;
 				}
 			}
 			if (temp == N - 1) {
-				count++;
+				result++;
 			}
 		}
-		
-		System.out.println(count);
+
+		System.out.println(result);
 	}
+
 }
