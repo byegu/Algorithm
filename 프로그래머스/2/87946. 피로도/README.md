a474de07cdf10d8e984a3b98f09ac000c5920965
byegu <100301088+byegu@users.noreply.github.com>
2026-05-21 16:03:15 +0900

    [level 2] Title: 피로도, Time: 2.63 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 60.4 MB, 시간: 5.09 ms
+메모리: 60.1 MB, 시간: 2.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 21일 15:40:24
+2026년 05월 21일 16:03:13
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,50 +1,26 @@
-import java.util.*;
-
 class Solution {
-    static int N, M;
-    static int max = 0;
     static boolean[] visited;
+    static int answer = 0;
+
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        N = dungeons.length;
-        M = dungeons[0].length;
-        visited = new boolean[N];
-        int[] list = new int[N];
-        Arrays.fill(list, -1);
-        
-        dfs(0, k, dungeons, new int[N]);
-        answer = max;
+        visited = new boolean[dungeons.length];
+
+        dfs(k, dungeons, 0);
+
         return answer;
     }
-    
-    public void dfs(int depth, int k, int[][] dungeons, int[] list) {
-        if (depth == N) {
-            int result = explore(list, k, dungeons);
-            max = Math.max(result, max);
-            return;
+
+    public void dfs(int fatigue, int[][] dungeons, int count) {
+        answer = Math.max(answer, count);
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i]) continue;
+
+            if (fatigue < dungeons[i][0]) continue;
+            
+            visited[i] = true;
+            dfs(fatigue - dungeons[i][1], dungeons, count + 1);
+            visited[i] = false;
         }
-        
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                list[depth] = i;
-                dfs(depth+1, k, dungeons, list);
-                visited[i] = false;
-            }
-        }
-        return;
-    }
-    
-    public int explore(int[] list, int k, int[][] dungeons) {
-        int result = 0;
-        int fatigue = k;
-        for (int i = 0; i < N; i++) {
-            int idx = list[i];
-            if (fatigue >= dungeons[idx][0]) {
-                fatigue -= dungeons[idx][1];
-                result++;
-            }
-        }
-        return result;
     }
 }
