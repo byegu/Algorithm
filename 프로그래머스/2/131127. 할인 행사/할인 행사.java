ff2637d52774dff375edc24e8bc0d9e6d182870e
byegu <100301088+byegu@users.noreply.github.com>
2026-08-15 13:55:08 +0900

    [level 2] Title: 할인 행사, Time: 29.14 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+
+        for (int i = 0; i <= discount.length - 10; i++) {
+            HashMap<String, Integer> map = new HashMap<>();
+
+            for (int j = i; j < i + 10; j++) {
+                map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
+            }
+
+            boolean check = true;
+
+            for (int j = 0; j < want.length; j++) {
+                if (map.getOrDefault(want[j], 0) != number[j]) {
+                    check = false;
+                    break;
+                }
+            }
+
+            if (check) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
