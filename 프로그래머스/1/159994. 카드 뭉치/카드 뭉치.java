7b7e3866bbf2522878259673927cc4530800a3bd
byegu <100301088+byegu@users.noreply.github.com>
2026-08-19 13:33:26 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,29 +1,18 @@
 class Solution {
-    static String result = "No";
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
+        int idx = 0;
+        int idx2 = 0;
         
-        dfs(cards1, cards2, goal, 0, 0, 0);
+        for (String word : goal) {
+            if (idx < cards1.length && cards1[idx].equals(word)) {
+                idx++;
+            } else if (idx2 < cards2.length && cards2[idx2].equals(word)) {
+                idx2++;
+            } else {
+                return "No";
+            }
+        }
         
-        return result;
-    }
-    
-    public void dfs(String[] cards1, String[] cards2, String[] goal, int idx, int idx2, int gidx) {
-
-        if (goal.length == gidx) {
-            result = "Yes";
-            return;
-        }
-
-        if (idx < cards1.length && cards1[idx].equals(goal[gidx])) {
-            dfs(cards1, cards2, goal, idx + 1, idx2, gidx + 1);
-            return;
-        }
-
-        if (idx2 < cards2.length && cards2[idx2].equals(goal[gidx])) {
-            dfs(cards1, cards2, goal, idx, idx2 + 1, gidx + 1);
-            return;
-        }
-        result = "No";
+        return "Yes";
     }
 }
